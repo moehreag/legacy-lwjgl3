d27b6b3ce27acbc70d7a304abaebcd7af852421c
moehreag <moehreag@gmail.com>
2023-11-09 15:51:07 +0100

    use ornithe toolchain

--- src/main/java/com/github/zarzelcow/legacylwjgl3/mixin/MixinMinecraftFixEarlyCrashNoReports.java
+++ src/main/java/com/github/zarzelcow/legacylwjgl3/mixin/MixinMinecraftFixEarlyCrashNoReports.java
@@ -2,7 +2,7 @@ package com.github.zarzelcow.legacylwjgl3.mixin;
 
 import com.mojang.blaze3d.platform.GLX;
 import net.minecraft.client.ClientBrandRetriever;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.resource.language.LanguageManager;
 import net.minecraft.client.world.ClientWorld;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(MinecraftClient.class)
+@Mixin(Minecraft.class)
 public class MixinMinecraftFixEarlyCrashNoReports {
     // @formatter:off
     @Shadow @Final private String gameVersion;
@@ -32,28 +32,28 @@ public class MixinMinecraftFixEarlyCrashNoReports {
      * @author mojang
      */
     @Overwrite
-    public CrashReport addSystemDetailsToCrashReport(CrashReport crashReport) {
-        crashReport.getSystemDetailsSection().add("Launched Version", () -> MixinMinecraftFixEarlyCrashNoReports.this.gameVersion);
-        crashReport.getSystemDetailsSection().add("LWJGL", Sys::getVersion);
+    public CrashReport populateCrashReport(CrashReport crashReport) {
+        crashReport.getSystemDetails().add("Launched Version", () -> MixinMinecraftFixEarlyCrashNoReports.this.gameVersion);
+        crashReport.getSystemDetails().add("LWJGL", Sys::getVersion);
         // check if gl is available in the current thread
         try {
             GL.getCapabilities();
-            crashReport.getSystemDetailsSection().add("OpenGL", () -> GL11.glGetString(7937) + " GL version " + GL11.glGetString(7938) + ", " + GL11.glGetString(7936));
+            crashReport.getSystemDetails().add("OpenGL", () -> GL11.glGetString(7937) + " GL version " + GL11.glGetString(7938) + ", " + GL11.glGetString(7936));
         } catch (IllegalStateException throwable) {
             // no-op if gl is not available
         }
-        crashReport.getSystemDetailsSection().add("GL Caps", GLX::getContextDescription);
-        crashReport.getSystemDetailsSection().add("Using VBOs", () -> MixinMinecraftFixEarlyCrashNoReports.this.options.vbo ? "Yes" : "No");
-        crashReport.getSystemDetailsSection().add("Is Modded", () -> {
+        crashReport.getSystemDetails().add("GL Caps", GLX::getGlCapsInfo);
+        crashReport.getSystemDetails().add("Using VBOs", () -> MixinMinecraftFixEarlyCrashNoReports.this.options.useVbo ? "Yes" : "No");
+        crashReport.getSystemDetails().add("Is Modded", () -> {
             String string = ClientBrandRetriever.getClientModName();
             if (!string.equals("vanilla")) {
                 return "Definitely; Client brand changed to '" + string + "'";
             } else {
-                return MinecraftClient.class.getSigners() == null ? "Very likely; Jar signature invalidated" : "Probably not. Jar signature remains and client brand is untouched.";
+                return Minecraft.class.getSigners() == null ? "Very likely; Jar signature invalidated" : "Probably not. Jar signature remains and client brand is untouched.";
             }
         });
-        crashReport.getSystemDetailsSection().add("Type", () -> "Client (map_client.txt)");
-        crashReport.getSystemDetailsSection().add("Resource Packs", () -> {
+        crashReport.getSystemDetails().add("Type", () -> "Client (map_client.txt)");
+        crashReport.getSystemDetails().add("Resource Packs", () -> {
             StringBuilder stringBuilder = new StringBuilder();
 
             for (String string : MixinMinecraftFixEarlyCrashNoReports.this.options.resourcePacks) {
@@ -69,11 +69,11 @@ public class MixinMinecraftFixEarlyCrashNoReports {
 
             return stringBuilder.toString();
         });
-        crashReport.getSystemDetailsSection().add("Current Language", () -> MixinMinecraftFixEarlyCrashNoReports.this.languageManager.getLanguage().toString());
-        crashReport.getSystemDetailsSection().add("Profiler Position", () -> MixinMinecraftFixEarlyCrashNoReports.this.profiler.enabled ? MixinMinecraftFixEarlyCrashNoReports.this.profiler.getCurrentLocation() : "N/A (disabled)");
-        crashReport.getSystemDetailsSection().add("CPU", GLX::getProcessor);
+        crashReport.getSystemDetails().add("Current Language", () -> MixinMinecraftFixEarlyCrashNoReports.this.languageManager.getLanguage().toString());
+        crashReport.getSystemDetails().add("Profiler Position", () -> MixinMinecraftFixEarlyCrashNoReports.this.profiler.isProfiling ? MixinMinecraftFixEarlyCrashNoReports.this.profiler.getCurrentLocation() : "N/A (disabled)");
+        crashReport.getSystemDetails().add("CPU", GLX::getCpuInfo);
         if (this.world != null) {
-            this.world.addToCrashReport(crashReport);
+            this.world.populateCrashReport(crashReport);
         }
 
         return crashReport;
