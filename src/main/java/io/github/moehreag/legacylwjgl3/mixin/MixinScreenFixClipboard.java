b1e8866189d3ec92e4c8f20cdaccd52ce3cb53df
moehreag <moehreag@gmail.com>
2024-02-03 01:41:51 +0100

    mc1.0.0

--- src/main/java/io/github/moehreag/legacylwjgl3/mixin/MixinScreenFixClipboard.java
+++ src/main/java/io/github/moehreag/legacylwjgl3/mixin/MixinScreenFixClipboard.java
@@ -5,7 +5,6 @@ import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(Screen.class)
 public abstract class MixinScreenFixClipboard {
@@ -17,14 +16,5 @@ public abstract class MixinScreenFixClipboard {
 	@Overwrite
 	public static String getClipboard(){
 		return GLFW.glfwGetClipboardString(Display.getHandle());
-	}
-
-	/**
-	 * @author moehreag
-	 * @reason Fix clipboard access with GLFW
-	 */
-	@Overwrite
-	public static void setClipboard(String string){
-		GLFW.glfwSetClipboardString(Display.getHandle(), string);
 	}
 }
