ef25934cc5d6731623daeaf447c392a19c160784
moehreag <moehreag@gmail.com>
2024-12-22 18:51:57 +0100

    (1.2.1) fix crash if the clipboard returns null values

--- src/main/java/io/github/moehreag/legacylwjgl3/mixin/MixinScreenFixClipboard.java
+++ src/main/java/io/github/moehreag/legacylwjgl3/mixin/MixinScreenFixClipboard.java
@@ -1,11 +1,12 @@
 package io.github.moehreag.legacylwjgl3.mixin;
 
+import java.util.Objects;
+
 import net.minecraft.client.gui.screen.Screen;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(Screen.class)
 public class MixinScreenFixClipboard {
@@ -16,7 +17,7 @@ public class MixinScreenFixClipboard {
 	 */
 	@Overwrite
 	public static String getClipboard(){
-		return GLFW.glfwGetClipboardString(Display.getHandle());
+        return Objects.requireNonNullElse(GLFW.glfwGetClipboardString(Display.getHandle()), "");
 	}
 
 	/**
