b91a98d1f0f9d905c1ca4576999a5b720eb4db2a
Floweynt <guywithanaxe42@gmail.com>
2025-03-27 13:11:55 -0500

    apply requested changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ pluginManagement {
         maven("https://maven.ornithemc.net/releases")
         maven("https://maven.ornithemc.net/snapshots")
         gradlePluginPortal()
-        maven("https://jitpack.io")
         maven("https://maven.wagyourtail.xyz/releases")
     }
 }
