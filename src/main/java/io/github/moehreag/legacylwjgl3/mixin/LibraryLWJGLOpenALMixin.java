0b6603e2c8ac511eccf1374aeef77003400a3028
moehreag <moehreag@gmail.com>
2025-04-03 16:23:44 +0200

    fix issues with OpenAL

---
+++ src/main/java/io/github/moehreag/legacylwjgl3/mixin/LibraryLWJGLOpenALMixin.java
@@ -0,0 +1,45 @@
+package io.github.moehreag.legacylwjgl3.mixin;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+import org.lwjgl.openal.AL;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import paulscode.sound.libraries.LibraryLWJGLOpenAL;
+
+@Mixin(LibraryLWJGLOpenAL.class)
+public class LibraryLWJGLOpenALMixin {
+
+	@Unique
+	private static MethodHandle alExit = null;
+
+	static {
+		try {
+			//noinspection JavaLangInvokeHandleSignature
+			alExit = MethodHandles.lookup().findStatic(AL.class, "exit", MethodType.methodType(void.class));
+		} catch (Exception ignored) {
+		}
+	}
+
+	@Redirect(method = "libraryCompatible", at = @At(value = "INVOKE", target = "Lorg/lwjgl/openal/AL;destroy()V"))
+	private static void libraryCompatible$replaceALDestroy() {
+		exitAL();
+	}
+
+	@Redirect(method = "cleanup", at = @At(value = "INVOKE", target = "Lorg/lwjgl/openal/AL;destroy()V"))
+	private void cleanup$replaceALDestroy() {
+		exitAL();
+	}
+
+	@Unique
+	private static void exitAL() {
+		try {
+			alExit.invoke();
+		} catch (Throwable ignored) {
+		}
+	}
+}
