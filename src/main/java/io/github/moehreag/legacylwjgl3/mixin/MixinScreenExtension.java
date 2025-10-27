1613739228417e3f8bb988c5f231af3e243021f8
moehreag <moehreag@gmail.com>
2025-10-27 12:01:15 +0100

    update gradle wrapper

--- src/main/java/io/github/moehreag/legacylwjgl3/mixin/MixinScreenExtension.java
+++ src/main/java/io/github/moehreag/legacylwjgl3/mixin/MixinScreenExtension.java
@@ -2,7 +2,6 @@ package io.github.moehreag.legacylwjgl3.mixin;
 
 import io.github.moehreag.legacylwjgl3.LegacyLWJGL3ScreenEx;
 import net.minecraft.client.gui.screen.Screen;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Screen.class)
