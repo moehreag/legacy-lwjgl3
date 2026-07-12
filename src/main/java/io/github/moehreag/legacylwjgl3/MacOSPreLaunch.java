b265a12316365c8eadb532a4ac70c64946ed8361
rdh <me@rdh540.dev>
2026-07-12 18:34:23 +0530

    use util.OS for detection

--- src/main/java/io/github/moehreag/legacylwjgl3/MacOSPreLaunch.java
+++ src/main/java/io/github/moehreag/legacylwjgl3/MacOSPreLaunch.java
@@ -1,11 +1,12 @@
 package io.github.moehreag.legacylwjgl3;
 
+import io.github.moehreag.legacylwjgl3.util.OS;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 public class MacOSPreLaunch implements PreLaunchEntrypoint {
 	@Override
 	public void onPreLaunch() {
-		if (System.getProperty("os.name").startsWith("Mac")) {
+		if (OS.current() == OS.OSX) {
 			// prevent AWT from taking over the AppKit event loop from GLFW/SDL
 			System.setProperty("java.awt.headless", "true");
 		}
