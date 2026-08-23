ed2ccae3b45ed0f947a0e3bfc4f1765e34d22f20
Saad Nadeem <saadndm.sn@gmail.com>
2026-08-23 17:32:38 -0400

    fix cached dependency loading

--- src/main/java/io/github/moehreag/legacylwjgl3/util/LibraryExtractor.java
+++ src/main/java/io/github/moehreag/legacylwjgl3/util/LibraryExtractor.java
@@ -2,17 +2,12 @@ package io.github.moehreag.legacylwjgl3.util;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.function.Consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.loader.api.FabricLoader;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.kamranzafar.jtar.TarEntry;
 import org.kamranzafar.jtar.TarInputStream;
 import org.tukaani.xz.XZInputStream;
@@ -27,15 +22,14 @@ public class LibraryExtractor {
 		try {
 			var container = FabricLoader.getInstance().getModContainer(MODID).orElseThrow();
 			var version = container.getMetadata().getVersion().getFriendlyString();
-			var out = Files.createDirectories(Path.of(System.getProperty("java.io.tmpdir"), MODID+"_"+version+"_dependencies"));
-			if (Files.exists(out)) return;
-			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-				try {
-					recursiveDelete(out);
-				} catch (Exception e) {
-					//log.warn("Failed to delete temp libraries dir!", e);
+			var out = Path.of(System.getProperty("java.io.tmpdir"), MODID+"_"+version+"_dependencies");
+			if (Files.exists(out)) {
+				try (var libraries = Files.list(out)) {
+					libraries.forEach(classpathAdder);
 				}
-			}));
+				return;
+			}
+			Files.createDirectories(out);
 			var path = container.findPath("libraries.tar.xz").orElseThrow(IOException::new);
 			try (var in = Files.newInputStream(path)) {
 
@@ -54,23 +48,5 @@ public class LibraryExtractor {
 		} catch (IOException e) {
 			log.error("Failed to extract libraries, the game is likely to crash!", e);
 		}
-	}
-
-	private void recursiveDelete(Path p) throws IOException {
-		if (!Files.exists(p)) return;
-		Files.walkFileTree(p, new SimpleFileVisitor<>() {
-			@Override
-			public @NotNull FileVisitResult visitFile(@NotNull Path file, @NotNull BasicFileAttributes attrs) throws IOException {
-				Files.deleteIfExists(file);
-				return super.visitFile(file, attrs);
-			}
-
-			@Override
-			public @NotNull FileVisitResult postVisitDirectory(@NotNull Path dir, @Nullable IOException exc) throws IOException {
-				Files.deleteIfExists(dir);
-				return super.postVisitDirectory(dir, exc);
-			}
-		});
-		Files.deleteIfExists(p);
 	}
 }
